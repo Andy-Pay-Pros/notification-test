b6945e5da2eac70c4f1c3519b5dc48b59ef22518
Andy-Pay-Pros <developer@pay-pros.com>
2024-10-21 14:25:13 -0300

    test whit mongodb

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cryptojs = require("crypto-js");
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://sancheztest97:1KbDa5m5lUAXFOmc@notification.qjury.mongodb.net/?retryWrites=true&w=majority&appName=notification";
+
 const app = express();
-const port = 3000;
+app.use(express.json());
+const port = 6000;
 
 const getKeyTripleDES = function (key) {
   let securityKeyArray = cryptojs.MD5(key).toString();
@@ -19,9 +24,31 @@ const decryptTripleDES = function (message, key) {
   return payload.toString(cryptojs.enc.Utf8);
 };
 
-app.post("/notifications", (req, res) => {
-  console.log(req);
-  return res.sendStatus(200);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let collection;
+
+app
+app.post("/notifications", async (req, res) => {
+  const notificationData = req.body;
+
+  if (!notificationData) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    const resultado = await collection.insertOne(notificationData);
+    return res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(400);
+  }
   //   try {
   //     // console.log(req.headers)
   //     console.log(req.body);
@@ -39,6 +66,18 @@ app.post("/notifications", (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server on port: ${port}`);
+  await connectToDB();
 });
+
+async function connectToDB() {
+  try {
+    await client.connect();
+    console.log("Conectado a MongoDB");
+    const database = client.db("notification");
+    collection = database.collection("notification");
+  } catch (err) {
+    console.error("Error conectando a la base de datos:", err);
+  }
+}
